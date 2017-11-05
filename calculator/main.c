9c9696f6515cd2e8d23bcc2e2f1bdc40b479683a
faridrr <farid.f1@hotmail.fr>
2017-11-05 15:42:13 +0100

    1

--- calculator/main.c
+++ calculator/main.c
@@ -4,12 +4,17 @@ int addition(int a,int b)
 {
     return a+b;
 }
+int soustraction(int a ,int b)
+{
+    return a-b;
+}
 
 int main()
 {
     printf("Hello world!\n");
 
     printf("l'addition de a et b vaut %d\n",addition(1,4));
+    printf("l'addition de a et b vaut %d\n",soustraction(1,4));
 
     return 0;
 }
