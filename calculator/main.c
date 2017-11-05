3033aff01e776aaeb79da11b71431bec757a5396
faridrr <farid.f1@hotmail.fr>
2017-11-05 15:14:15 +0100

    r

--- calculator/main.c
+++ calculator/main.c
@@ -8,10 +8,8 @@ int addition(int a,int b)
 int main()
 {
     printf("Hello world!\n");
-<<<<<<< HEAD
-    printf("a+b= %d\n",addition(1,4));
-=======
+
     printf("l'addition de a et b vaut %d\n",addition(1,4));
->>>>>>> 8cd20ce8d76e6b384fc0d7db20a5cb0be744f079
+
     return 0;
 }
